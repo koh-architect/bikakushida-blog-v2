ce84e2d33db5a6ac3e02618819121f134b6bc529
関谷虹 <kosekiya@NijiMBP14.local>
2026-03-31 13:20:29 +0900

    fix base trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://koh-architect.github.io',
-  base: '/bikakushida-blog-v2',
+  base: '/bikakushida-blog-v2/',
 });
