01432a4d5264bc593219e7de086963e9ef3e977b
関谷虹 <kosekiya@NijiMBP14.local>
2026-04-02 12:27:34 +0900

    Fix Astro site and base for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://koh-architect.github.io',
-  base: '/bikakushida-blog-v2/',
+  site: 'https://bikakushida.jp',
 });
